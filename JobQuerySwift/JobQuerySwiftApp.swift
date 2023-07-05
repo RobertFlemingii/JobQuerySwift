6520a5899c9012709107f08db84f2b3e816e677b
Robert Fleming <robertefleming.ca@gmail.com>
2023-07-04 20:03:07 -0700

    first commmit

---
+++ JobQuerySwift/JobQuerySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobQuerySwiftApp.swift
+//  JobQuerySwift
+//
+//  Created by Robert Edward Fleming on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct JobQuerySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JobQuerySwiftUITests/JobQuerySwiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  JobQuerySwiftUITestsLaunchTests.swift
+//  JobQuerySwiftUITests
+//
+//  Created by Robert Edward Fleming on 7/4/23.
+//
+
+import XCTest
+
+final class JobQuerySwiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
